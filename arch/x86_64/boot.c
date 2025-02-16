7aef99399d7a93242c222c5e911e939f4cf99cef
oito8bits <williandossantosdeoliveira287@gmail.com>
2025-02-15 23:47:26 -0300

    Fix boot_init()

--- arch/x86_64/boot.c
+++ arch/x86_64/boot.c
@@ -8,8 +8,8 @@ efi_memory_descriptor map[4096];
 void boot_init(struct boot_info *info)
 {
   memcpy(&boot_info, info, sizeof(struct boot_info));
-  memcpy(&map, &info->map.memory_descriptor, info->map.memory_map_size);
-  boot_info.map.memory_descriptor = map + MM_KERNEL_VIRTUAL_BASE;
+  memcpy(map, info->map.memory_descriptor, info->map.memory_map_size);
+  boot_info.map.memory_descriptor = map;
   boot_info.map.memory_map_size = info->map.memory_map_size;
 }
 
