18d285372eb14f46d3aa51cdd548f2fe323ca077
oito8bits <williandossantosdeoliveira287@gmail.com>
2025-02-24 22:23:30 -0300

    Add zone and area struct

--- include/mm/zones.h
+++ include/mm/zones.h
@@ -2,6 +2,22 @@
 #define _INCLUDE_ZONES_
 
 #include <ant/boot.h>
+#include <ant/types.h>
+#include <ant/list.h>
+
+#define AREA_ORDER 11
+
+struct area
+{
+  struct list_head list;
+  u32 nareas;
+};
+
+struct zone
+{
+  u32 npages;
+  struct area area[AREA_ORDER];
+};
 
 void zones_init(struct memory_map *);
 
