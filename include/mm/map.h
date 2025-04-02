7374e966c6ed32f67301f8179d13f57f7627265c
oito8bits <williandossantosdeoliveira287@gmail.com>
2025-04-02 20:41:43 -0300

    Add function for get number of memory in memory

---
+++ include/mm/map.h
@@ -0,0 +1,9 @@
+#ifndef _INCLUDE_MAP_
+#define _INCLUDE_MAP_
+
+#include <ant/types.h>
+#include <ant/boot.h>
+
+size_t map_get_memory_pages(struct memory_map *);
+
+#endif
---
+++ mm/map.c
@@ -0,0 +1,15 @@
+#include <mm/map.h>
+
+size_t map_get_memory_pages(struct memory_map *map)
+{
+  efi_memory_descriptor *descriptor = map->memory_descriptor;
+  size_t i;
+  size_t pages = 0;
+  for(i = 0; i < map->memory_map_size / map->descriptor_size; i++)
+  {
+    descriptor = (void *) descriptor + map->descriptor_size;
+    pages += descriptor->number_of_pages;
+  }
+
+  return pages;
+}
