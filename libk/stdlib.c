506b5bc6927ceaa98fddb4baa3b271b1ee57302f
oito8bits <williandossantosdeoliveira287@gmail.com>
2025-04-14 20:40:12 -0300

    Fix utoa()

--- libk/stdlib.c
+++ libk/stdlib.c
@@ -4,13 +4,21 @@
 void utoa(char *source, u64 number)
 {
   char *string = source;
+
+  if(!number)
+  {
+    *string++ = '0';
+    *string = '\0';
+    return;
+  }
+
   while(number)
   {
     *string++ = number % 10 + '0';
     number /= 10;
   }
 
-  *++string = '\0';
+  *string = '\0';
 
   strrev(source);
 }
