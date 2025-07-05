bd0c8b008664cd40fca6ad451776197b6e5c110e
oito8bits <williandossantosdeoliveira287@gmail.com>
2025-07-04 22:17:22 -0300

    Add IS_ALIGN()

--- include/ant/align.h
+++ include/ant/align.h
@@ -1,6 +1,7 @@
 #ifndef _INCLUDE_ALIGN_
 #define _INCLUDE_ALIGN_
 
+#define IS_ALIGN(value, align) ((value) & ((align) - 1))
 #define ALIGNUP(value, align) ((value) + ((align) - 1) & ~((align) - 1))
 
 #endif
