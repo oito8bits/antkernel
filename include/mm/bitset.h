99f8d25423c5369d3b288daa79b5dc4f761d2c6a
oito8bits <williandossantosdeoliveira287@gmail.com>
2025-08-16 20:55:29 -0300

    Add macro and fix unsigned long value

--- include/mm/bitset.h
+++ include/mm/bitset.h
@@ -3,6 +3,8 @@
 
 #include <ant/types.h>
 
+#define BITS_PER_ENTRY 64
+
 u64 bitset_is_set(u64 *bitset, size_t pos);
 void bitset_set(u64 *bitset, size_t pos);
 void bitset_reset(u64 *bitset, size_t pos);
--- mm/bitset.c
+++ mm/bitset.c
@@ -2,20 +2,20 @@
 
 static u64 calculate_bit_pos(size_t pos)
 {
-  return 1U << pos % 64;
+  return 1UL << pos % BITS_PER_ENTRY;
 }
 
 u64 bitset_is_set(u64 *bitset, size_t pos)
 {
-  return bitset[pos / 64] & calculate_bit_pos(pos);
+  return bitset[pos / BITS_PER_ENTRY] & calculate_bit_pos(pos);
 }
 
 void bitset_set(u64 *bitset, size_t pos)
 {
-  bitset[pos / 64] |= calculate_bit_pos(pos); 
+  bitset[pos / BITS_PER_ENTRY] |= calculate_bit_pos(pos); 
 }
 
 void bitset_reset(u64 *bitset, size_t pos)
 {
-  bitset[pos / 64] &= ~calculate_bit_pos(pos);
+  bitset[pos / BITS_PER_ENTRY] &= ~calculate_bit_pos(pos);
 }
