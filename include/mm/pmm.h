b43e46fa9dd700214fe8dc52d08bad16abdb0371
oito8bits <williandossantosdeoliveira287@gmail.com>
2025-08-17 20:10:14 -0300

    Alloc reserved pages

--- include/mm/pmm.h
+++ include/mm/pmm.h
@@ -24,7 +24,7 @@ struct pmm_area
 void pmm_init_area(struct area *, phys_addr_t, size_t);
 void *pmm_alloc_area(struct area *);
 void *pmm_alloc_page(struct area *);
-void pmm_alloc_range(struct area *, phys_addr_t, size_t);
+s64 pmm_alloc_addr(phys_addr_t);
 void pmm_free_page(struct area *, void *);
 void pmm_init(struct boot_info *, struct pmm_area *);
 
--- mm/pmm.c
+++ mm/pmm.c
@@ -7,19 +7,40 @@
 #include <mm/bitset.h>
 #include <arch/x86_64/pg.h>
 #include <ant/align.h>
+#include <ant/list.h>
 
-static size_t addr_to_idx(struct area *area, phys_addr_t addr)
+static u64 *pages;
+static size_t npages;
+static size_t nentries;
+
+static size_t addr_to_idx(phys_addr_t addr)
 {
-  return (addr - area->start) / PAGE_SIZE;
+  return addr / PAGE_SIZE;
 }
 
-void pmm_init_area(struct area *area, phys_addr_t start, size_t npages)
+static void alloc_reserved_pages(struct memory_map *map)
 {
-  area->npages = npages;
-  area->nentries = npages / 64;
+  efi_memory_descriptor *descriptor = map->memory_descriptor;
+  size_t i, j;
+  for(i = 0; i < map->memory_map_size / map->descriptor_size; i++)
+  {
+    if(memmap_get_memory_type(descriptor->type) == available_memory)
+      goto out;
+     
+    for(j = 0; j < descriptor->number_of_pages; j++)
+      pmm_alloc_addr(descriptor->physical_start + j * PAGE_SIZE);
+
+    out:
+    descriptor = (void *) descriptor + map->descriptor_size;
+  }
+}
+
+void pmm_init_area(struct area *area, phys_addr_t start, size_t area_npages)
+{
+  area->npages = area_npages;
+  area->nentries = area_npages / 64;
   area->start = start;
-  area->pages = early_malloc(npages * 8);
-  memset(area->pages, 0, area->nentries * 8);
+  area->pages = pages + start / PAGE_SIZE / 64;
 }
 
 void *pmm_alloc_area(struct area *area)
@@ -36,7 +57,7 @@ void *pmm_alloc_area(struct area *area)
       if(!bitset_is_set(area->pages, pos))
       {
         bitset_set(area->pages, pos);
-        return (void *) area->start + pos * PAGE_SIZE;
+        return (void *) (area->start + pos * PAGE_SIZE);
       }
     }
   }
@@ -49,33 +70,32 @@ void *pmm_alloc_page(struct area *area)
   return pmm_alloc_area(area);
 }
 
-size_t alloc_addr(struct area *area, phys_addr_t addr)
+s64 pmm_alloc_addr(phys_addr_t addr)
 {
-  size_t idx = addr_to_idx(area, addr);
-  if(!bitset_is_set(area->pages, idx))
+  size_t idx = addr_to_idx(addr);
+  if(!bitset_is_set(pages, idx))
   {
-    bitset_set(area->pages, idx);
+    bitset_set(pages, idx);
     return idx;
   }
 
   return -1;
 }
 
-void pmm_alloc_range(struct area *area, phys_addr_t addr, size_t npages)
-{
-  size_t i, idx;
-  for(i = 0; i < npages; i++)
-    alloc_addr(area, addr + i * PAGE_SIZE);
-}
-
 void pmm_free_page(struct area *area, void *addr)
 {
-  bitset_reset(area->pages, addr_to_idx(area, (phys_addr_t) addr));
+  bitset_reset(area->pages, addr_to_idx((phys_addr_t) addr));
 }
 
 void pmm_init(struct boot_info *boot_info, struct pmm_area *parea)
 {
-  //size_t npages = memmap_get_memory_pages(&boot_info->map);
+  npages = memmap_get_memory_pages(&boot_info->map);
+  nentries = npages / 64;
+  pages = early_malloc(npages * 8);
+  memset(pages, 0, nentries * 8);
+  
+  alloc_reserved_pages(&boot_info->map);
+  
   pmm_init_area(&parea->kernel_area, 
                 boot_info->kernel_entry,
                 boot_info->kernel_size / PAGE_SIZE);
@@ -83,7 +103,7 @@ void pmm_init(struct boot_info *boot_info, struct pmm_area *parea)
   phys_addr_t table_area_size = boot_info->kernel_entry + boot_info->kernel_size;
   if(IS_ALIGN(table_area_size, PAGE_SIZE))
     table_area_size = ALIGNUP(table_area_size, PAGE_SIZE);
-
+  
   pmm_init_area(&parea->table_area,
                 table_area_size,
                 20 * (1 << 20) / PAGE_SIZE); /* 20 MiB */
