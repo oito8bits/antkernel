88d120841c07785bce1a5cf16ac883a4855652cd
oito8bits <williandossantosdeoliveira287@gmail.com>
2025-09-06 20:05:05 -0300

    Remove unnecessary function

--- mm/vmm.c
+++ mm/vmm.c
@@ -51,21 +51,6 @@ static void map_kernel(void)
   pg_switch_top_table(pg_virt_to_phys((virt_addr_t) &kernel_top_table));
 }
 
-static void free_available_pages(struct memory_map *map)
-{
-/*
-  memset(pages, 0, nentries * 8);
-
-  efi_memory_descriptor *descriptor = map->memory_descriptor;
-
-  size_t i;
-  for(i = 0; i < map->memory_map_size / map->descriptor_size; i++)
-  {
-
-  }
-*/
-}
-
 void vmm_get_pages(size_t npages)
 {
 }
