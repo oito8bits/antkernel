beb49d8baa203a527334544f35abfa600d2de938
oito8bits <williandossantosdeoliveira287@gmail.com>
2025-09-30 19:24:19 -0400

    Fix madt signature

--- include/drivers/acpi/acpi.h
+++ include/drivers/acpi/acpi.h
@@ -7,7 +7,18 @@
 #define XSDP_SIGNATURE 0x2052545020445352
 #define XSDT_SIGNATURE 0x54445358
 
-#define MADT_SIGNATURE 0x7843495041
+#define MADT_SIGNATURE 0x43495041
+
+enum madt_type
+{
+  LAPIC,
+  IOAPIC,
+  IOAPIC_INT_SOURCE_OVERRIDE,
+  IOAPIC_NMI_SOURCE,
+  LAPIC_NMI,
+  LAPIC_ADDRESS_OVERRIDE,
+  PROCESSOR_LOCAL_APIC_X2APIC
+};
 
 struct rsdp
 {
@@ -61,6 +72,35 @@ struct ioapic
   u8 reserved;
   u32 ioapic_address;
   u32 global_sys_interrupt_base;
+} __attribute__ ((packed));
+
+struct madt
+{
+  u8 entry_type;
+  u8 record_length;
+} __attribute__ ((packed));
+
+struct madt_lapic
+{
+  struct madt madt;
+  u8 acpi_processor_id;
+  u8 acpi_id;
+  u32 flags;
+} __attribute__ ((packed));
+
+struct madt_ioapic
+{
+  struct madt madt;
+  u8 ioapic_id;
+  u8 reserved;
+  u32 ioapic_addr;
+  u32 global_sys_int_base;
+} __attribute__ ((packed));
+
+struct madt_apic
+{
+  u32 local_apic_addr;
+  u32 flags;
 } __attribute__ ((packed));
 
 void acpi_init(struct boot_info *);
