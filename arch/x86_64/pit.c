26b7acd6421a3090ae10a65a67820cfe4530676b
oito8bits <williandossantosdeoliveira287@gmail.com>
2025-10-07 19:37:41 -0400

    Create a sleep() with pit

---
+++ arch/x86_64/pit.c
@@ -0,0 +1,18 @@
+#include "pit.h"
+#include "io.h"
+
+void pit_sleep(u32 ms)
+{
+  io_outb(0x43, 0x30);
+  u16 count = 1193;
+  while(ms)
+  {
+    io_outb(0x40, count & 0xff);
+    io_outb(0x40, (count >> 8) & 0xff);
+    do
+    {
+      io_outb(0x43, 0xe2);
+    } while(!(io_inb(0x40) & (1 << 7)));
+    ms--;
+  }
+}
---
+++ arch/x86_64/pit.h
@@ -0,0 +1,8 @@
+#ifndef _INCLUDE_PIT_
+#define _INCLUDE_PIT_
+
+#include <ant/types.h>
+
+void pit_sleep(u32);
+
+#endif
