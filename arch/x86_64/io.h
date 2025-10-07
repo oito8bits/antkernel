377f60bce2348cd994958c0c98e51d2b22fad0b5
oito8bits <williandossantosdeoliveira287@gmail.com>
2025-10-07 19:35:49 -0400

    Fix io functions

--- arch/x86_64/io.h
+++ arch/x86_64/io.h
@@ -1,11 +1,24 @@
 #ifndef _INCLUDE_IO_
 #define _INCLUDE_IO_
 
-void io_out(unsigned short port, unsigned char value)
+#include <ant/types.h>
+
+static inline void io_outb(u16 port, u8 value)
 {
 	__asm__ __volatile__(
-			"outb %%al, %%dx"
-			: : "r" (value), "r" (port));
+			"outb %0, %1"
+			: : "a" (value), "Nd" (port));
+}
+
+static inline u16 io_inb(u16 port)
+{
+  u8 result;
+
+  __asm__ __volatile__(
+      "inb %1, %0"
+      : "=a" (result): "Nd" (port));
+
+  return result;
 }
 
 #endif
