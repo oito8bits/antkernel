e088b99c10513268340fa0371abd6348af77ba75
oito8bits <williandossantosdeoliveira287@gmail.com>
2025-10-23 22:41:40 -0400

    Implement ioapic

--- arch/x86_64/ioapic.c
+++ arch/x86_64/ioapic.c
@@ -6,6 +6,22 @@
 struct madt_ioapic *madt_ioapic;
 u32 *ioapic_base;
 
+struct iored
+{
+  union
+  {
+    struct
+    {
+      u8 vector;
+      u8 flags;
+      u8 mask;
+      u32 reserved;
+      u8 target;
+    };
+    u64 raw_iored;
+  };
+} __attribute__((packed));
+
 extern struct table_entry kernel_top_table;
 
 static u32 read_reg(int reg)
@@ -20,13 +36,45 @@ static void write_reg(int reg, u32 value)
   ioapic_base[4] = value;
 }
 
+static u64 read_redirection(int irq)
+{
+  u64 lo = read_reg(0x10 + 2 * irq);
+  u64 hi = read_reg(0x11 + 2 * irq);
+  return hi << 32 | lo;
+}
+
+static void write_redirection(int irq, struct iored *iored)
+{
+  write_reg(0x10 + 2 * irq, iored->raw_iored);
+  write_reg(0x11 + 2 * irq, iored->raw_iored >> 32);
+}
+
 void ioapic_init(void)
 {
   ioapic_base = (u32 *) KERNEL_IOAPIC_BASE;
   madt_ioapic = acpi_get_ioapic();
+  // Map ioapic register area.
   map_pages(&kernel_top_table,
             (phys_addr_t) madt_ioapic->ioapic_addr,
             ioapic_base,
             BIT_PRESENT | BIT_WRITE | BIT_CACHE_DISABLE | (1UL << 8),
             PAGE_SIZE);
+
+  // Set iored entries.
+  size_t i;
+  struct iored iored;
+  for(i = 0; i < 2; i++)
+  {
+     iored.raw_iored = read_redirection(i);
+     iored.vector = 33 + i;
+     iored.flags = 0x0;
+     iored.mask |= 1;
+     iored.target = 0x0;
+     write_redirection(i, &iored);
+  }
+
+  // Unmask keyboard.
+  iored.raw_iored = read_redirection(1);
+  iored.mask &= ~0x1;
+  write_redirection(1, &iored);
 }
