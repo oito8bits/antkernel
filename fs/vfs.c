78f3e105736092738e3e80e9177075d298b6a072
oito8bits <williandossantosdeoliveira287@gmail.com>
2025-11-13 18:19:47 -0500

    Fix and improve vfs

--- fs/vfs.c
+++ fs/vfs.c
@@ -2,6 +2,7 @@
 #include <mm/heap.h>
 #include <libk/string.h>
 #include <fs/ramfs/ramfs.h>
+#include <fs/devfs/devfs.h>
 #include <fs/mp.h>
 
 struct vfs_fd file_descriptors[4096];
@@ -11,7 +12,7 @@ static int get_new_file_descriptor(void)
   size_t i;
   for(i = 0; i < 4096; i++)
   {
-    if(file_descriptors[i].fs_fd < 0)
+    if(!file_descriptors[i].free)
       return i;
   }
 
@@ -23,34 +24,34 @@ int vfs_open(const char *path, int flags)
   struct mountpoint *mp = mp_search(path);
   if(mp == NULL)
     return -1;
-
   int fd = get_new_file_descriptor();
-  if(mp->ops->open(path, flags) == NULL)
+  
+  file_descriptors[fd].mp = mp;
+  
+  if(mp->ops->open(&file_descriptors[fd], path, flags) < 0)
     return -1;
-
-  file_descriptors[fd] =  (struct vfs_fd)
-                          {.fs_fd = 0,
-                          .ops = mp->ops};
+ 
+  file_descriptors[fd].free = 1;
   
   return fd;
 }
 
 size_t vfs_read(int fd, void *buffer, size_t size)
 {
-  return file_descriptors[fd].ops->read(&file_descriptors[fd], buffer, size);
+  return file_descriptors[fd].mp->ops->read(&file_descriptors[fd], buffer, size);
 }
 
 size_t vfs_write(int fd, void *buffer, size_t size)
 {
-  return file_descriptors[fd].ops->write(&file_descriptors[fd], buffer, size);
+  return file_descriptors[fd].mp->ops->write(&file_descriptors[fd], buffer, size);
 }
 
 int vfs_close(int fd)
 {
-  if(file_descriptors[fd].ops->close(&file_descriptors[fd]) == -1)
+  if(file_descriptors[fd].mp->ops->close(&file_descriptors[fd]) == -1)
     return -1;
 
-  file_descriptors[fd].fs_fd = -1;
+  file_descriptors[fd].free = 1;
 
   return 0;
 }
@@ -74,8 +75,9 @@ int vfs_init(void)
 
   size_t i = 0;
   for(i = 0; i < 4096; i++)
-    file_descriptors[i].fs_fd = -1;
+    file_descriptors[i].free = 0;
 
+  devfs_init();
   ramfs_init();
 
   return 0;
