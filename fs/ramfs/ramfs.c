66951811aea92963b353e1712156b4a91dc047b0
oito8bits <williandossantosdeoliveira287@gmail.com>
2025-11-19 06:07:56 -0500

    Add ramfs_open()

--- fs/ramfs/ramfs.c
+++ fs/ramfs/ramfs.c
@@ -2,6 +2,10 @@
 #include <fs/ramfs/ramdisk.h>
 #include <fs/vfs.h>
 #include <mm/vmm.h>
+#include <libk/string.h>
+#include <ant/align.h>
+
+#define BLOCK_SIZE 512
 
 struct vfs_ops tar_ops =
 {
@@ -14,6 +18,21 @@ struct vfs_ops tar_ops =
   .umount = ramfs_umount
 };
 
+struct ramfs_fd
+{
+  size_t absolute_offset;
+};
+
+static u64 octal_to_dec(char *source)
+{
+  u64 result = 0;
+  size_t i;
+  for(i = 0; source[i]; i++)
+    result = result * 8 + source[i] - '0'; 
+
+  return result;
+} 
+
 struct vfs_ops *ramfs_get_ops(void)
 {
   return &tar_ops;
@@ -21,7 +40,37 @@ struct vfs_ops *ramfs_get_ops(void)
 
 int ramfs_open(struct vfs_fd *fd, const char *path, int flags)
 {
-  vfs_open(fd->mp->device, 0);
+  int dev_fd = vfs_open(fd->mp->device, 0);
+  if(dev_fd < 0)
+    return dev_fd;
+
+  path++;
+
+  vfs_lseek(dev_fd, 0, VFS_SEEK_SET);
+
+  size_t name_len, path_len;
+  struct ramfs_tar_header *header;
+  char buffer[BLOCK_SIZE];
+  do
+  {
+    vfs_read(dev_fd, buffer, BLOCK_SIZE);
+    header = (struct ramfs_tar_header *) buffer;
+    strcpy(header->name, path);
+    
+    name_len = strlen(header->name);
+    path_len = strlen(path);
+    
+    size_t file_size = BLOCK_SIZE;
+    if(!strncmp(header->name, path, name_len))
+    {
+      vfs_lseek(dev_fd, file_size, VFS_SEEK_CUR);
+      continue;
+    }
+  } while(name_len != path_len);
+  
+  fd->fs_file_info = header;
+  
+  return 0;
 }
 
 size_t ramfs_write(struct vfs_fd *fd, void *buffer, size_t count)
