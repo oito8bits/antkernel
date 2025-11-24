96c17073837bd3bd0f611fcdb63e0a60862bfa2e
oito8bits <williandossantosdeoliveira287@gmail.com>
2025-11-23 22:47:02 -0300

    Fix ramfs_open(), add ramfs_read() and ramfs_write()

--- fs/ramfs/ramfs.c
+++ fs/ramfs/ramfs.c
@@ -2,6 +2,7 @@
 #include <fs/ramfs/ramdisk.h>
 #include <fs/vfs.h>
 #include <mm/vmm.h>
+#include <mm/heap.h>
 #include <libk/string.h>
 #include <ant/align.h>
 
@@ -40,16 +41,18 @@ struct vfs_ops *ramfs_get_ops(void)
 
 int ramfs_open(struct vfs_fd *fd, const char *path, int flags)
 {
-  int dev_fd = vfs_open(fd->mp->device, 0);
+  int dev_fd = fd->mp->dev_fd;
   if(dev_fd < 0)
     return dev_fd;
 
   path++;
 
+  size_t old_offset = vfs_lseek(dev_fd, 0, VFS_SEEK_CUR);
   vfs_lseek(dev_fd, 0, VFS_SEEK_SET);
 
   size_t name_len, path_len;
   struct ramfs_tar_header *header;
+  struct ramfs_fd *ramfs_fd = heap_malloc(sizeof(struct ramfs_fd));
   char buffer[BLOCK_SIZE];
   do
   {
@@ -67,22 +70,47 @@ int ramfs_open(struct vfs_fd *fd, const char *path, int flags)
       continue;
     }
   } while(name_len != path_len);
-  
-  fd->fs_file_info = header;
-  
+  ramfs_fd->absolute_offset = vfs_lseek(dev_fd, 0, VFS_SEEK_CUR) - BLOCK_SIZE;
+  fd->fs_file_info = ramfs_fd;
+#include <libk/kprintf.h>
+  kprintf("abosulte_offset: %li\n", ramfs_fd->absolute_offset);
+  vfs_lseek(dev_fd, old_offset, VFS_SEEK_SET);
+
   return 0;
 }
 
 size_t ramfs_write(struct vfs_fd *fd, void *buffer, size_t count)
 {
+  int dev_fd = fd->mp->dev_fd;
+  struct ramfs_fd *ramfs_fd = fd->fs_file_info;
+  size_t old_offset = vfs_lseek(dev_fd, 0, VFS_SEEK_CUR);
+  vfs_lseek(dev_fd, ramfs_fd->absolute_offset, VFS_SEEK_CUR);
+  size_t size = vfs_write(dev_fd, buffer, count);
+  vfs_lseek(dev_fd, old_offset, VFS_SEEK_SET);
+  
+  return size;
 }
 
 size_t ramfs_read(struct vfs_fd *fd, void *buffer, size_t count)
 {
+  int dev_fd = fd->mp->dev_fd;
+  struct ramfs_fd *ramfs_fd = fd->fs_file_info;
+  size_t old_offset = vfs_lseek(dev_fd, 0, VFS_SEEK_CUR);
+  vfs_lseek(dev_fd, ramfs_fd->absolute_offset, VFS_SEEK_CUR);
+  size_t size = vfs_read(dev_fd, buffer, count);
+  vfs_lseek(dev_fd, old_offset, VFS_SEEK_SET);
+
+  return size;
 }
 
 int ramfs_close(struct vfs_fd *fd)
 {
+  if(fd->fs_file_info)
+    return 0;
+
+  heap_free(fd->fs_file_info);
+
+  return 0;
 }
 
 int ramfs_mount(struct mountpoint *mp)
