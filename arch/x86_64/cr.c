011e0647f7825a03960f31e78d3dfd61cfc272bc
oito8bits <williandossantosdeoliveira287@gmail.com>
2025-12-26 20:45:11 -0300

    Organize code

--- arch/x86_64/cr.c
+++
@@ -1,6 +0,0 @@
-#include "cr.h"
-
-void cr_set_cr3(u64 top_table)
-{
-  __asm__ volatile("mov    %0, %%cr3" : : "r"(top_table));
-}
--- arch/x86_64/cr.h
+++ arch/x86_64/cr.h
@@ -3,6 +3,15 @@
 
 #include <ant/types.h>
 
-void cr_set_cr3(u64 top_table);
+static inline void cr_set_cr3(u64 top_table)
+{
+  __asm__ volatile("mov    %0, %%cr3" : : "r"(top_table));
+}
+
+static inline u64 cr_get_cr4(void)
+{
+  u64 cr4;
+  __asm__ volatile("movq  %%cr4, %0": "=r" (cr4));
+}
 
 #endif
