765d32907225bff13bf2b7dad0a9db2b46ffc0a6
oito8bits <williandossantosdeoliveira287@gmail.com>
2026-01-27 23:00:45 -0300

    Fix context switch

--- arch/x86_64/context.c
+++ arch/x86_64/context.c
@@ -1,34 +1,15 @@
 #include <arch/x86_64/context.h>
+#include <libk/string.h>
 #include "tss.h"
 
 void context_copy(struct context *dst, struct context *src)
 {
-  dst->rip = src->rip; 
-  dst->rsp = src->rsp;
-  dst->cs = src->cs;
-  dst->ss = src->ss;
-  /*
-  new->r9 = old->r9; 
-  new->r10 = old->r10; 
-  new->r11 = old->r11; 
-  new->r12 = old->r12; 
-  new->r13 = old->r13; 
-  new->r14 = old->r14; 
-  new->r15 = old->r15; 
-  new->rbp = old->rbp; 
-  new->rbx = old->rbx; 
-  new->rax = old->rax; 
-  new->rcx = old->rcx; 
-  new->rdx = old->rdx; 
-  new->rsi = old->rsi; 
-  new->rdi = old->rdi; 
-  new->rflags = old->rflags; 
-  */
+  memcpy(dst, src, sizeof(struct context));
 }
 
 void context_set_kstack(void *kstack_top)
 {
   struct tss *tss = tss_get_base();
   
-  tss->rsp0 = kstack_top;
+  tss->rsp0 = (u64) kstack_top;
 }
